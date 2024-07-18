358709131cdc2a7c55fb19d1a913e39b3c08370a
Alper Balbay <itsc0508@gmail.com>
2024-07-18 06:18:03 +0300

    modifications to make it work with 'net.py', 'processing.py', and 'tokenizers.py'

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import argparse
 
 import torch
 
-from net import HierarchicalModel, Model
+from net import HierarchicalMLP, MLP
 from nnutils import generate_text, train_loop
-from preprocessing import load_data, process_data
+from processors import CharLevelMLPProcessor
+from tokenizers import CharTokenizer
 
 parser = argparse.ArgumentParser(
     description="Train a neural net with given file to generate text"
 )
+parser.add_argument("--train_ratio", type=float, default=0.8, help="ratio of the input data that will be used for the training")
 parser.add_argument("--file", type=str, help="Path to the file")
 parser.add_argument("--n_embed", type=int, default=15, help="Embedding size")
 parser.add_argument("--n_hidden", type=int, default=400, help="Hidden size")
@@ -27,7 +29,7 @@ parser.add_argument(
     "--n_chars", type=int, default=100, help="Number of characters to generate"
 )
 parser.add_argument(
-    "--model", type=str, default="h", help="(h)ierarchical or (m)lp model to train"
+    "--model", type=str, default="hmlp", help="(hmlp) hierarchical, (mlp) mlp, or (gpt) GPT model to train"
 )
 parser.add_argument(
     "--n_consecutive",
@@ -60,17 +62,23 @@ if __name__ == "__main__":
         )
         print(text)
     else:
-        # read the file
-        text_list, vocab_size = load_data(args["file"])
-        # construct the dataset and get the vocabulary size
-        text = "".join(text_list).lower()
-        vocab_size = len(set(text)) + 1
+        if args['model'] in ["hmlp", "mlp"]:
+            tokenizer = CharTokenizer()
+            processor = CharLevelMLPProcessor(
+                paths=args['file'],
+                tokenizer=tokenizer,
+                context_length=args['block_size'])
+        elif args['model'] == "gpt":
+            pass
 
-        train_loader, test_loader = process_data(
-            text, block_size=args["block_size"], batch_size=args["batch_size"]
-        )
-        if args["model"] == "h":
-            model = HierarchicalModel(
+        train_loader, test_loader = processor.get_dataloaders(args['batch_size'],
+                                                              args['train_ratio'])
+        
+        vocab_size = tokenizer.vocab_size + 1
+        print("vocab_size:", vocab_size)
+
+        if args["model"] == "hmlp":
+            model = HierarchicalMLP(
                 vocab_size,
                 n_consecutive=args["n_consecutive"],
                 n_embed=args["n_embed"],
@@ -78,14 +86,15 @@ if __name__ == "__main__":
                 n_layers=args["n_layers"],
                 block_size=args["block_size"],
             )
-        elif args["model"] == "m":
-            model = Model(
+        elif args["model"] == "mlp":
+            model = MLP(
                 vocab_size,
                 n_embed=args["n_embed"],
                 block_size=args["block_size"],
                 n_hidden=args["n_hidden"],
                 n_layers=args["n_layers"],
             )
+
         train_loop(
             model,
             train_loader,
