51d149c54fd4da95210afb41eba8a2a68807cf32
Alper Balbay <itsc0508@gmail.com>
2024-07-18 08:38:27 +0300

    the vocabulary enumeration starts from the number of special tokens now, it's not hardcoded anymore

--- tokenizers.py
+++ tokenizers.py
@@ -88,7 +88,7 @@ class CharTokenizer(Tokenizer):
         # itos: ix -> string
         self.stoi = self.special_tokens.copy()
         self.stoi.update(
-            {t: i for i, t in enumerate(tokens, 2)}
+            {t: i for i, t in enumerate(tokens, len(self.special_tokens))}
         )  # +2 for the special tokens
         self.itos = {i: t for t, i in self.stoi.items()}
 
