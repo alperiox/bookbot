f50194d0bed15994608d3bcaae763a25fe6f0c18
Alper Balbay <itsc0508@gmail.com>
2024-07-18 14:28:17 +0300

    added docstrings, removed generate_text function

--- nnutils.py
+++ nnutils.py
@@ -6,6 +6,7 @@ from utils import load_artifacts, save_artifacts
 
 
 def optimize_step(parameters, learning_rate=0.001):
+    """optimize the model parameters just once using SGD."""
     for param in parameters:
         param.data += -learning_rate * param.grad
 
@@ -21,28 +22,46 @@ def evaluate(model, x, y):
 
 
 def train_loop(model, train_loader, test_loader, epochs, learning_rate, lrsche):
-    # a single training loop
+    """
+    Trains the given model using the provided data loaders.
+
+    model: the model to be trained, current available ones are MLP, hMLP, and GPT.
+    train_loader (torch DataLoader): the training loader that loads the training batches for the training
+    test_loader (torch DataLoader) : the testing loader that loads the data batches for the testing phase
+    epochs (int): number of epochs (iterations) to train the model
+    learning_rate (float): the learning rate that'll be used to scale the gradients in the optimization phase
+    lrsche (bool): whether apply a learning rate decay or not
+    """
+    # allow the model parameters to calculate gradients
     parameters = model.parameters()
     for p in parameters:
         p.requires_grad = True
 
+    # loss vectors
     train_losses = torch.zeros(epochs)
     valid_losses = torch.zeros(epochs)
+
+    # the initial learning_rate
     lr = learning_rate
+
+    # training loop
     for epoch in range(epochs):
+        # set up the tqdm bar
         bar = tqdm(enumerate(train_loader), total=len(train_loader))
+        # decay the learning rate if it's provided
         if lrsche:
             if epoch == int(epochs * 0.5):
                 lr = lr / 10
         print("========")
         print("TRAINING (epoch:%d/%d)" % (epoch + 1, epochs))
         for i, (x, y) in bar:
+            # get the logits and the loss
             logits, loss = model(x, y)
-
+            # backward pass
             loss.backward()
             optimize_step(parameters, lr)
             loss = loss.item()
-
+            # statistics and loggign
             train_losses[epoch] += loss
             desc_text = f"({epoch*train_loader.batch_size + i*train_loader.batch_size}/{len(train_loader.dataset)}) (lr={lr:.4f}): loss {train_losses.sum()/(i+1):.4f}"
             bar.set_description(desc_text)
@@ -51,45 +70,12 @@ def train_loop(model, train_loader, test_loader, epochs, learning_rate, lrsche):
         bar = tqdm(enumerate(test_loader), total=len(test_loader))
         for i, (x, y) in bar:
             with torch.no_grad():
+                # same as above, don't calculate the gradients this time
+                # and just calculate the loss
                 logits, loss = model(x, y)
-
+            # statistics and logging, again.
             valid_losses[epoch] += loss
             desc_text = f"({epoch*test_loader.batch_size + i*test_loader.batch_size}/{len(test_loader.dataset)}): loss {valid_losses.sum()/(i+1):.4f}"
             bar.set_description(desc_text)
-
+    # return the losses.
     return train_losses, valid_losses
-
-
-def generate_text(
-    seed_text, model=None, char_to_ix=None, ix_to_char=None, max_new_tokens=100
-):
-    if not model:
-        model = load_artifacts("model")["model"]
-    if not char_to_ix:
-        char_to_ix = load_artifacts("char_to_ix")["char_to_ix"]
-    if not ix_to_char:
-        ix_to_char = load_artifacts("ix_to_char")["ix_to_char"]
-
-    model.eval()
-    with torch.no_grad():
-        num_generated_chars = 0
-        generated_text = seed_text
-        # pad or truncate the seed text to the block size
-        if len(generated_text) > model.block_size:
-            input_text = generated_text[: model.block_size]
-        else:
-            input_text = " " * (model.block_size - len(generated_text)) + generated_text
-
-        while num_generated_chars < max_new_tokens and generated_text[-1] != "|":
-            input_block = [char_to_ix[char] for char in input_text]
-            input_tensor = torch.tensor(input_block, dtype=torch.long).unsqueeze(0)
-            out = model(input_tensor)
-            # sample the next character
-            next_char_ix = torch.multinomial(F.softmax(out, dim=1), 1).item()
-            next_char = ix_to_char[next_char_ix]
-            generated_text += next_char
-            input_text = input_text[1:] + next_char
-
-            num_generated_chars += 1
-
-    return generated_text.strip("|")
